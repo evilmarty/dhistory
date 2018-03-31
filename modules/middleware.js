f6ad10a5730a1d8b2ccfb584bb00d76b0aa5335c
Marty Zalega <marty@zalega.me>
2018-03-31 20:34:57 +1000

    Move actionFor and pathFor inside

--- modules/middleware.js
+++ modules/middleware.js
@@ -27,21 +27,6 @@ function findPattern(patterns, path, { strict, exact, sensitive }) {
   return matches[0];
 }
 
-const actionFor = (patterns, lookup) => (path, options = DEFAULT_MATCH_PATH_OPTIONS) => {
-  const match = findPattern(patterns, path, options) || NULL_PATTERN_MATCH;
-  const action = lookup[match.path];
-
-  return action ? newAction(action, match.params) : null;
-};
-
-const pathFor = lookup => action => {
-  const pattern = lookup[action.type];
-  const keys = Object.keys(action).filter(key => key !== 'type');
-  const params = keys.reduce((memo, key) => Object.assign(memo, { [key]: action[key] }), {});
-
-  return pattern ? generatePath(pattern, params) : null;
-};
-
 export default history => routes => {
   if (typeof(routes) !== 'object') {
     throw new TypeError('routes is not an Object');
@@ -51,14 +36,26 @@ export default history => routes => {
   const actionsByPatterns = patterns.reduce((memo, pattern) => Object.assign(memo, { [pattern]: normaliseAction(routes[pattern]) }), {});
   const patternsByActionTypes = patterns.reduce((memo, pattern) => Object.assign(memo, { [actionsByPatterns[pattern].type]: pattern }), {});
 
+  const pathFor = action => {
+    const pattern = patternsByActionTypes[action.type];
+    const keys = Object.keys(action).filter(key => key !== 'type');
+    const params = keys.reduce((memo, key) => Object.assign(memo, { [key]: action[key] }), {});
+
+    return pattern ? generatePath(pattern, params) : null;
+  };
+
+  const actionFor = (path, options = DEFAULT_MATCH_PATH_OPTIONS) => {
+    const match = findPattern(patterns, path, options) || NULL_PATTERN_MATCH;
+    const action = actionsByPatterns[match.path];
+
+    return action ? newAction(action, match.params) : null;
+  };
+
   return store => {
     let bypassUpdate = false;
 
-    store.pathFor = pathFor(patternsByActionTypes);
-    store.actionFor = actionFor(patterns, actionsByPatterns);
-
     history.listen(location => {
-      const action = store.actionFor(location.pathname);
+      const action = actionFor(location.pathname);
 
       if (action) {
         bypassUpdate = true;
@@ -69,7 +66,7 @@ export default history => routes => {
 
     return next => action => {
       if (!bypassUpdate) {
-        const path = store.pathFor(action);
+        const path = pathFor(action);
 
         if (path !== null) {
           history[action.replace ? 'replace' : 'push'](path);
